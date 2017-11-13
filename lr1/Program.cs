0b27c2fb855955554f6bf952e5d58cf7194f723d
oxotnik96 <oxotnik.96@mail.ru>
2017-11-13 10:35:08 +0200

    start
    start

---
+++ lr1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        interface ILanguage
+        {
+            void Build();
+            void Execute();
+        }
+    }
+}
